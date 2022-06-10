07c23f265a142850911e03f95bacaa6a66d8041e
Kristijan Aleksic <kristijan.aleksic@gmail.com>
2022-06-10 14:51:32 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+const PORT = 3001
+const url = "mongodb+srv://ninja:ljeto2022@cluster0.svfrbat.mongodb.net/?retryWrites=true&w=majority"
+
+//Connect to DB
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+
+    //set template engine
+    app.set('view engine', 'ejs')
+    //Middleware
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.listen(process.env.PORT || PORT, (req, res) => {
+    console.log(`Server is now running on port ${PORT}`)
+    })
+
+    //CRUD handlers
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+          .then(results => {
+            res.render('index.ejs', { quotes: results })
+          })
+          .catch(/* ... */)
+      })
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(err => console.log(err))
+    })
+  })
+
+
+
